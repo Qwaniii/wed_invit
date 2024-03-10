012a29a2b50e823c6ecdcaaabf21321403bf4605
Qwaniii <wxolms@gmail.com>
2024-03-10 23:34:14 +0300

    fix screen mobile, ymaps

--- js/maps.js
+++ js/maps.js
@@ -1,8 +1,25 @@
+let isMobile = {
+    Android: function() {return navigator.userAgent.match(/Android/i);},
+    BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+    iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+    Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+    Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
 ymaps.ready(init);
 function init(){
     let myMap = new ymaps.Map("map", {
         center: [55.765802, 37.644561],
         zoom: 17,
-        controls: ['smallMapDefaultSet']
+        controls: ['smallMapDefaultSet'],
+        // behaviors: ["drag", "dblClickZoom", "rightMouseButtonMagnifier", "multiTouch"]  
     });
+
+    if(isMobile.any()){
+        myMap.behaviors.disable('scrollZoom');
+        myMap.behaviors.disable('drag');
+}
 }
