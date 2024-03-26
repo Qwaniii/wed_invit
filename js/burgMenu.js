972a5eb0bc7f18a2d51cba24c270f8479455f248
Qwaniii <115154547+Qwaniii@users.noreply.github.com>
2024-03-26 21:51:19 +0000

    upd burger + background block img

--- js/burgMenu.js
+++ js/burgMenu.js
@@ -1,15 +1,23 @@
 let burger = document.querySelector('.menu-burger__header'),
     menu = document.querySelector('.menu'),
     wrapper = document.querySelector('.menu-wrapper');
+let burgerToggle = false;
+let j = 0;
 
 burger.addEventListener('click', function(e) {
   menu.classList.toggle('active');
   wrapper.classList.toggle('active');
+  body.classList.toggle("popup-active")
+  burgerToggle = !burgerToggle
+  burgerToggle ? window.history.pushState(null, null, '') : window.history.back() 
 });
 
 wrapper.addEventListener('click', function(e) {
   menu.classList.toggle('active');
   wrapper.classList.toggle('active');
+  body.classList.toggle("popup-active")
+  window.history.back()
+  burgerToggle = false
 })
 
 // закрытие окна меню по esc 
@@ -18,20 +26,35 @@ document.addEventListener("keydown", (e) => {
   if(e.code == "Escape" && menu.classList.contains('active')) {
     menu.classList.toggle('active');
     wrapper.classList.toggle('active');
+    body.classList.toggle("popup-active")
+    window.history.back()
+    burgerToggle = false
   }
 })
+
+window.addEventListener("popstate", (event) => {
+  event.preventDefault();
+  if (menu.classList.contains('active')){
+    menu.classList.toggle('active');
+    wrapper.classList.toggle('active');
+    body.classList.toggle("popup-active")
+    burgerToggle = false
+  }
+});
+
 
 // плавная прокрутка
 
 function addSmoothScroll(anchor) {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
- 
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
     });
     menu.classList.toggle('active');
     wrapper.classList.toggle('active');
+    body.classList.toggle("popup-active")
+    burgerToggle = false
   });
 }
  
