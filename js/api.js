f066a9e2fb5ebf527da48c145ff20b6ea5babaa6
Qwaniii <wxolms@gmail.com>
2024-04-09 18:07:24 +0300

    upd

--- js/api.js
+++ js/api.js
@@ -1,6 +1,8 @@
 class Api {
     constructor() {
+        // send to private 
         // this.url = "https://api.telegram.org/bot7063713067:AAEOTHGN_x-StvgR7SGEUxcxTBOJKg6qXgs/sendMessage?chat_id=5200592838&text=";
+        // send to group chat
         this.url = "https://api.telegram.org/bot7063713067:AAEOTHGN_x-StvgR7SGEUxcxTBOJKg6qXgs/sendMessage?chat_id=-1002131519980&text=";
     }
 
