157da97d2904fcb1998d8a3091f9824c3f119c4b
Qwaniii <115154547+Qwaniii@users.noreply.github.com>
2024-05-01 20:18:28 +0000

    bc

--- js/accordeon.js
+++ js/accordeon.js
@@ -6,19 +6,19 @@ boxes.forEach((box) => {
 
 function boxHandler(e) {
   e.preventDefault(); // сбрасываем стандартное поведение
-  console.log(e)
 
   let currentBox = e.target.closest(".color-wrapper"); // определяем текущий бокс
   let currentContent = e.target.nextElementSibling; // находим скрытый контент
   if (e.target.classList.contains("label_wed")) {
     currentBox.classList.toggle("active"); // присваиваем ему активный класс
+    if (currentBox.classList.contains("active")) {
+      // если класс активный ..
+    
+      currentContent.style.maxHeight = currentContent.scrollHeight + "px"; // открываем контент
+      } else {
+        // в противном случае
+        currentContent.style.maxHeight = 0; // скрываем контент
+      }
   }
-  if (currentBox.classList.contains("active")) {
-  // если класс активный ..
 
-  currentContent.style.maxHeight = currentContent.scrollHeight + "px"; // открываем контент
-  } else {
-    // в противном случае
-    currentContent.style.maxHeight = 0; // скрываем контент
-  }
 }
